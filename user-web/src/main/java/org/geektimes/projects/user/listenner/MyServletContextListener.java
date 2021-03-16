e2b4ec1bfb242c70aef3f6b5c9f82b1a2be2f916
jxdwugx <571098146@qq.com>
2021-03-16 21:29:57 +0800

    第三次作业提交

--- user-web/src/main/java/org/geektimes/projects/user/listenner/MyServletContextListener.java
+++ user-web/src/main/java/org/geektimes/projects/user/listenner/MyServletContextListener.java
@@ -1,10 +1,15 @@
 package org.geektimes.projects.user.listenner;
 
 import org.geektimes.projects.user.context.ComponentContext;
+import org.geektimes.projects.user.domain.User;
+import org.geektimes.projects.user.mbean.UserManager;
 
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.lang.management.ManagementFactory;
 
 /**
  * @description
@@ -21,6 +26,7 @@ public class MyServletContextListener implements ServletContextListener {
         servletContext = servletContextEvent.getServletContext();
         ComponentContext context = new ComponentContext();
         context.init(servletContext);
+        registerMBean();
     }
 
     @Override
@@ -28,5 +34,22 @@ public class MyServletContextListener implements ServletContextListener {
 
     }
 
+    private void registerMBean(){
+        // 获取平台 MBean Server
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        try {
+            // 为 UserMXBean 定义 ObjectName
+            ObjectName objectName = new ObjectName("org.geektimes.projects.user.mbean:type=User");
+            // 创建 UserMBean 实例
+            User user = new User();
+            mBeanServer.registerMBean(createUserMBean(user), objectName);
+        }catch (Exception e){
+            servletContext.log("注册MBean失败", e);
+        }
 
+    }
+
+    private static Object createUserMBean(User user) {
+        return new UserManager(user);
+    }
 }
